1f082bbf4380de0b4bb7ad85038d92317d57c911
Justin Fitzsimmons <justin@fitzsimmons.ca>
2021-10-01 20:45:09 -0400

    Improves README

--- example-frontend/index.js
+++ example-frontend/index.js
@@ -1,15 +1,13 @@
 import * as solver from "ffxiv-static-solver";
 
-const button = document.getElementById("activate")
-
 const solve = () => {
   const definitions = document.getElementById("definitions").value
   const job_preferences = document.getElementById("job_preferences").value
   const desired_composition = document.getElementById("desired_composition").value
 
-  console.log(job_preferences)
-
+  console.time("solve")
   const result = solver.solve(definitions, desired_composition, job_preferences)
+  console.timeEnd("solve")
 
   document.getElementById("results").innerText = result
 }
